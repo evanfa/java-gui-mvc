828718232421daeb959c0b541c7d3a3dbf06ae2d
evanfa <fabio.a.rodriguez@gmail.com>
2022-08-10 22:19:16 -0500

    Commets to Manuel

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome Manuel");
+
+        System.out.println("Manuel con el intellij trata de ir a la carpeta src. Ponle new -> Swing UI Designer -> Form");
+        System.out.println("Ahi puedes crear una interfaz grafica para despues irle agregando la funcionalidad");
+
     }
 }
